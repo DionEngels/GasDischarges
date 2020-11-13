be445cf0f03ae26f1c49ece44c57f37309280b83
DionEngels <d.j.engels@student.tue.nl>
2020-11-13 10:36:04 +0100

    Add sqr

--- example_field/main.cpp
+++ example_field/main.cpp
@@ -4,7 +4,7 @@
 int main() {
   Field field(10);
   for (unsigned i = 0; i < field.size(); ++i) {
-    field[i] = i * i;
+    field[i] = sqr(i);
   }
   field.write(std::cerr);
   field.plot("position", "value", "example field");
