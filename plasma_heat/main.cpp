f5575d71e9dd4b5cde8d63a3bac09da7a4332d59
DionEngels <d.j.engels@student.tue.nl>
2020-11-24 15:01:21 +0100

    Ex5.5

--- plasma_heat/main.cpp
+++ plasma_heat/main.cpp
@@ -3,17 +3,22 @@
 #include <fstream>
 #include <iostream>
 
+// declare function
+double heavy_heat_conductivity(double T_h);
+
 int main(int argc, char **argv) {
   const unsigned N = 16;
   const double x_e = 0.0;
   const double x_w = 0.001;
   const double T_w = 300.0;
   const double V = 0.0;
-  const double lambda = 0.318;
+  double T_central = 2000.0;
+  double lambda;
   const double Sc = 1.02 * pow(10, 10);
   const double Sp = 0.0;
   const double k_b = 1.380658e-23;
-  const double beta_cv = 2.5 * 5 * pow(10, 23) * k_b;
+  const double beta_cv = 2.5 * 5e23 * k_b;
+  const int n_iterations = 10;
 
   Grid grid(N, x_e, x_w, Grid::Cylindrical);
 
@@ -26,15 +31,31 @@ int main(int argc, char **argv) {
 
   temp.beta_cv = beta_cv;
   for (unsigned i = 0; i < grid.num_np(); ++i) {
-    temp.lambda[i] = lambda;
     temp.sc[i] = Sc;
     temp.sp[i] = Sp;
   }
 
-  temp.Update();
+  for (int i = 1; i < n_iterations + 1; i++) {
+    lambda = heavy_heat_conductivity(T_central); // update lambda
+    for (unsigned i = 0; i < grid.num_np(); ++i) {
+      temp.lambda[i] = lambda; // update PhiVariable
+    }
+    temp.Update();       // solve
+    T_central = temp[0]; // set new T_central
+    std::cout << "Iteration " << i << "\t Current T_central " << T_central
+              << std::endl;
+  }
 
-  grid.write(std::cerr, temp);
-  grid.plot(temp, "position (m)", "temperature (K)", "Exercise 5.3");
+  grid.plot(temp, "position (m)", "temperature (K)", "Exercise 5.5");
 
   return 0;
+}
+
+// implement function
+double heavy_heat_conductivity(double T_h) {
+  const double k_b = 1.380658e-23;
+  const double m_h = 6.623e-26;
+  const double s_aa = 6.33e-20;
+
+  return sqrt(8.0 * k_b * T_h / (M_PI * m_h)) * sqrt(2) / s_aa * k_b;
 }
