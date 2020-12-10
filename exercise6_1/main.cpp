b27d7e130a6d5f2e6c97f33698ec81218df0625a
Dion Engels <d.j.engels@student.tue.nl>
2020-12-10 09:09:49 +0100

    Exercise 6.1

---
+++ exercise6_1/main.cpp
@@ -0,0 +1,51 @@
+#include "../3diagsys.h"
+#include "../field.h"
+#include "grid.h"
+#include "physconst.h"
+#include <iostream>
+
+int main() {
+  // 1. Configuration
+
+  unsigned N = 20;                // number of points;
+  const double L = 0.01;          // grid size
+  const double del = L / (N - 1); // distance between adjacent grid points
+  const double S = 1e-4;          // source density
+  const double e0 = PhysConst::epsilon0; // e0
+
+  const double V_w = 0;   // value at west boundary.
+  const double V_e = 100; // value at east boundary.
+
+  // 2. Set up the variables
+
+  Field dens(N);
+  Grid grid(N - 2, 0, L, Grid::Cartesian);
+  TridiagonalSystem sys(N);
+
+  // 3. Discretise the equations
+
+  // West BC:
+  sys.ap(0) = 1;
+  sys.aw(0) = 0;
+  sys.b(0) = V_w;
+
+  // East BC:
+  sys.ap(N - 1) = 1;
+  sys.ae(N - 1) = 0;
+  sys.b(N - 1) = V_e;
+
+  // Interior points: -Dd^2n/dx^2 = S.
+  for (unsigned i = 1; i < N - 1; ++i) {
+    sys.ap(i) = +2 * e0 / (del * del);
+    sys.aw(i) = +1 * e0 / (del * del);
+    sys.ae(i) = +1 * e0 / (del * del);
+    sys.b(i) = S;
+  }
+
+  // 4. Solve the systeem of equations
+  sys.solve(dens);
+
+  grid.plot(dens, "position (m)", "Voltage (V)", "Exercise 6.1");
+
+  return 0;
+}
