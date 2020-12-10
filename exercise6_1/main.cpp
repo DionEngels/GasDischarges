8fd4a86c1f6f8d2010d3b4342f2e6caa6f1db5f8
Dion Engels <d.j.engels@student.tue.nl>
2020-12-10 09:14:12 +0100

    Exercise 6.2

--- exercise6_1/main.cpp
+++ exercise6_1/main.cpp
@@ -1,5 +1,5 @@
-#include "../3diagsys.h"
-#include "../field.h"
+#include "3diagsys.h"
+#include "field.h"
 #include "grid.h"
 #include "physconst.h"
 #include <iostream>
@@ -12,6 +12,7 @@ int main() {
   const double del = L / (N - 1); // distance between adjacent grid points
   const double S = 1e-4;          // source density
   const double e0 = PhysConst::epsilon0; // e0
+  const double e_r = 11.68;
 
   const double V_w = 0;   // value at west boundary.
   const double V_e = 100; // value at east boundary.
@@ -34,18 +35,26 @@ int main() {
   sys.ae(N - 1) = 0;
   sys.b(N - 1) = V_e;
 
-  // Interior points: -Dd^2n/dx^2 = S.
-  for (unsigned i = 1; i < N - 1; ++i) {
+  // Interior points (left)
+  for (unsigned i = 1; i < N / 2; ++i) {
     sys.ap(i) = +2 * e0 / (del * del);
     sys.aw(i) = +1 * e0 / (del * del);
     sys.ae(i) = +1 * e0 / (del * del);
     sys.b(i) = S;
   }
 
+  // Interior points (right)
+  for (unsigned i = N / 2; i < N - 1; ++i) {
+    sys.ap(i) = +2 * e0 * e_r / (del * del);
+    sys.aw(i) = +1 * e0 * e_r / (del * del);
+    sys.ae(i) = +1 * e0 * e_r / (del * del);
+    sys.b(i) = S;
+  }
+
   // 4. Solve the systeem of equations
   sys.solve(dens);
 
-  grid.plot(dens, "position (m)", "Voltage (V)", "Exercise 6.1");
+  grid.plot(dens, "position (m)", "Voltage (V)", "Exercise 6.2");
 
   return 0;
 }
