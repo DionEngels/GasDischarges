9311c625bb77c999d6e89f6d7f35a22b62492b1f
Dion Engels <d.j.engels@student.tue.nl>
2020-12-14 11:14:36 +0100

    Change cross-section

--- argon.h
+++ argon.h
@@ -8,7 +8,7 @@
 //		  @article{Phe1999:1,
 //		   author="A. V. Phelps and Z. Lj. Petrovi{\'c}",
 //		   title="Review Article: Cold-Cathode discharges and breakdown
-//		          in argon: surface and gas phase production of 
+//		          in argon: surface and gas phase production of
 //		          secondary electrons",
 //		   journal="Plasma Sources Sci. Technol."i,
 //		   year="1999",
@@ -20,154 +20,122 @@
 #ifndef H_ARGON_H
 #define H_ARGON_H
 
-#include <cmath>
 #include "crosssec.h"
 #include "physconst.h"
+#include <cmath>
 
-namespace Argon
-{
+
+namespace Argon {
 
 // Mass in kilogram.
-double Mass() { return 39.948*PhysConst::AMU; }
+double Mass() { return 39.948 * PhysConst::AMU; }
 
 // ionization energy in Joule
 double E_ion() { return 15.759 * PhysConst::e; }
-	
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= 
+
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // Now some implementations.
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= 
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Crosssection (m^2) for elastic "e + Ar -> e + Ar" collision as a
 // function of the electron energy in Joule.
-class Elastic : public CrossSection
-{
-  public:
-	Elastic()
-	 : CrossSection(0.0)
-	{}
-  protected:
-	double get_above_th(double eps) const
-	{
-		using std::pow;
-		const double eV = 1.6022e-19;
-		eps /= eV; // the lines below want eps in eV.
-		if (eps > 100.0) eps = 100.0;
-		double sigma = std::fabs(6.0/pow((1.0 + (eps/0.1) + pow((eps/0.6),2)),3.3)
-			- 1.1*pow(eps,1.4)/(1.0+pow((eps/15.0),1.2))/
-					sqrt(1.0+pow((eps/5.5), 2.5)
-			+ pow((eps/60.0),4.1))) + 0.05/pow((1.0+(eps/10.0)),2.0)
-			+ 0.01*pow(eps,3.0)/(1.0+pow((eps/12),6.0));
-		return (1e-20*sigma);
-	}
+class Elastic : public CrossSection {
+public:
+  Elastic() : CrossSection(1e-19) {}
+
+protected:
+  double get_above_th(double eps) const { return (1e-19); }
 };
 
 // Crosssection (m^2) for inelastic "e + Ar -> e + Ar*" collision as
 // a function of the electron energy in Joule.
-class Inelastic : public CrossSection
-{
-  public:
-	Inelastic()
-	 : CrossSection(11.5*PhysConst::eV)
-	{}
-  protected:
-	double get_above_th(double eps) const
-	{
-		using std::pow;
-		const double eV = 1.6022e-19;
-		eps /= eV; // the lines below want eps in eV.
-		if (eps > 100.0) eps = 100.0;
-		double sigma = 0.0;
-		if (eps>=11.5) 
-		{
-			sigma = 0.034*pow((eps-11.5),1.1)
-				*(1.0+pow((eps/15.0),2.8))
-				/(1.0+pow((eps/23.0),5.5)) + 0.023*(eps-11.5)
-				/(1.0+pow((eps/80.0),1.9));
-		}
-		return (1e-20*sigma);
-	}
+class Inelastic : public CrossSection {
+public:
+  Inelastic() : CrossSection(11.5 * PhysConst::eV) {}
+
+protected:
+  double get_above_th(double eps) const {
+    using std::pow;
+    const double eV = 1.6022e-19;
+    eps /= eV; // the lines below want eps in eV.
+    if (eps > 100.0)
+      eps = 100.0;
+    double sigma = 0.0;
+    if (eps >= 11.5) {
+      sigma = 0.034 * pow((eps - 11.5), 1.1) * (1.0 + pow((eps / 15.0), 2.8)) /
+                  (1.0 + pow((eps / 23.0), 5.5)) +
+              0.023 * (eps - 11.5) / (1.0 + pow((eps / 80.0), 1.9));
+    }
+    return (1e-20 * sigma);
+  }
 };
 
 // Crosssection (m^2) for ionisation "e + Ar -> 2e + Ar+" collision as
 // a function of the electron energy in Joule.
-class Ionisation : public CrossSection
-{
-  public:
-	Ionisation()
-	 : CrossSection(15.8*PhysConst::eV)
-	{}
-  protected:
-	double get_above_th(double eps) const
-	{
-		using std::pow;
-		const double eV = 1.6022e-19;
-		eps /= eV; // the lines below want eps in eV.
-		if (eps > 100.0) eps = 100.0;
-		double sigma = 0.0;
-		if (eps>=15.8) 
-		{
-		        sigma = 970.0*(eps-15.8)/pow((70.0+eps),2.0)
-				+ 0.06*pow((eps-15.8),2.0)*exp(-eps/9.0);
-		}
-		return (1e-20*sigma);
-	}
+class Ionisation : public CrossSection {
+public:
+  Ionisation() : CrossSection(15.8 * PhysConst::eV) {}
+
+protected:
+  double get_above_th(double eps) const {
+    using std::pow;
+    const double eV = 1.6022e-19;
+    eps /= eV; // the lines below want eps in eV.
+    if (eps > 100.0)
+      eps = 100.0;
+    double sigma = 0.0;
+    if (eps >= 15.8) {
+      sigma = 970.0 * (eps - 15.8) / pow((70.0 + eps), 2.0) +
+              0.06 * pow((eps - 15.8), 2.0) * exp(-eps / 9.0);
+    }
+    return (1e-20 * sigma);
+  }
 };
 
 // Crosssection (m^2) for charge exchange "Ar+ + Ar -> Ar + Ar+"
 // collision.
-class ChargeExchange : public CrossSection
-{
-  public:
-	ChargeExchange()
-	 : CrossSection(0.0)
-	{}
-  protected:
-	double get_above_th(double eps) const
-	{
-		const double eV = 1.6022e-19;
-		eps /= eV; // the lines below want eps in eV.
-		if (eps > 100.0) eps = 100.0;
-		double sigma;
-		if (eps < 4.0) 
-		{
-			sigma = -2.95e-19*std::sqrt(eps)+10.65e-19;
-		}
-		else
-		{
-			sigma = 2.0e-19 + 5.5e-19/(std::sqrt(eps)+1.0e-30);
-		}
-		return sigma;
-	}
+class ChargeExchange : public CrossSection {
+public:
+  ChargeExchange() : CrossSection(0.0) {}
+
+protected:
+  double get_above_th(double eps) const {
+    const double eV = 1.6022e-19;
+    eps /= eV; // the lines below want eps in eV.
+    if (eps > 100.0)
+      eps = 100.0;
+    double sigma;
+    if (eps < 4.0) {
+      sigma = -2.95e-19 * std::sqrt(eps) + 10.65e-19;
+    } else {
+      sigma = 2.0e-19 + 5.5e-19 / (std::sqrt(eps) + 1.0e-30);
+    }
+    return sigma;
+  }
 };
 
 // Crosssection (m^2) for elastic "Ar+ + Ar -> Ar+ + Ar"
 // collision.
-class IonElastic : public CrossSection
-{
-  public:
-	IonElastic()
-	 : CrossSection(0.0)
-	{}
-  protected:
-	double get_above_th(double eps) const
-	{
-		const double eV = 1.6022e-19;
-		eps /= eV; // the lines below want eps in eV.
-		if (eps > 100.0) eps = 100.0;
-		double sigma;
-		if (eps < 4.0)
-		{
-			sigma = -2.0e-19*std::sqrt(eps)+7.8e-19;
-		}
-		else
-		{
-			sigma = 1.8e-19 + 4.0e-19/(std::sqrt(eps)+1.0e-30);
-		}
-		return sigma;
-	}
+class IonElastic : public CrossSection {
+public:
+  IonElastic() : CrossSection(0.0) {}
+
+protected:
+  double get_above_th(double eps) const {
+    const double eV = 1.6022e-19;
+    eps /= eV; // the lines below want eps in eV.
+    if (eps > 100.0)
+      eps = 100.0;
+    double sigma;
+    if (eps < 4.0) {
+      sigma = -2.0e-19 * std::sqrt(eps) + 7.8e-19;
+    } else {
+      sigma = 1.8e-19 + 4.0e-19 / (std::sqrt(eps) + 1.0e-30);
+    }
+    return sigma;
+  }
 };
 
-} // End of Argon:: namespace
-
+} // namespace Argon
 
 #endif // H_ARGON_H
